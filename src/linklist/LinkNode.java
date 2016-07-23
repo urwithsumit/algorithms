6d6dde0d40de27a1fd13c91b1e469469ee5abcda
urwithsumit <sumitkumar@192.168.0.14>
2016-07-23 16:27:12 -0700

    Link Node. Generic for all Linkedlist related programs

---
+++ src/linklist/LinkNode.java
@@ -0,0 +1,14 @@
+package linklist;
+
+/**
+ * LinkList Node class. Will use the same for single linklist and double link list implementation
+ * 
+ * @author sumitkumar
+ * 
+ * @param <T>
+ */
+public class LinkNode<T> {
+	public T data;
+	public LinkNode<T> next;
+	public LinkNode<T> previous;
+}
